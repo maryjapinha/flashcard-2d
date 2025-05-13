f9896550b193f27546701e0a9a14a8d020ddaebc
maryjapinha <marieli.fernandes.pires@escola.pr.gov.br>
2025-05-13 14:28:17 +0000

    ok

---
+++ perguntas.js
@@ -0,0 +1,19 @@
+    criaCartao(
+        'programação',
+        'o que é HTML',
+        'linguagem de marcação'
+    )
+
+        criaCartao(
+        categoria,
+        pergunta,
+        resposta
+    )
+
+         criaCartao(
+            ingles
+            Como é dia do pagamento
+        payday,
+        pergunta,
+        resposta
+    )
