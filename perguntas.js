59a39b3f86ce43c67fb353768483f40079633a6d
maryjapinha <marieli.fernandes.pires@escola.pr.gov.br>
2025-06-10 14:27:43 +0000

    ok

--- perguntas.js
+++ perguntas.js
@@ -5,15 +5,15 @@
     )
 
         criaCartao(
-        categoria,
-        pergunta,
-        resposta
-    )
+        `matemática`,
+        `qual e é o número pi?`,
+        `3,14`
+
 
          criaCartao(
             ingles
             Como é dia do pagamento
-        payday,
-        pergunta,
-        resposta
+        `geografia`,
+        `o que é planice?`,
+        `Uma planície é uma área geográfica grande, com pouca ou nenhuma variação de altitude.`
     )
